5e96b6eacd3dfa477350b398ffda30d3af27e904
jmackie4 <jordan.mackie48@googlemail.com>
2016-11-02 17:38:00 +0000

    Bump.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='activityio',
-    version='0.0.2',
+    version='0.0.3',
     description='Exercise data handling library',
     long_description=long_description,
     url='https://github.com/jmackie4/activityio',
